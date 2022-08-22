8adf2f9b3190d9f08ae2abe488e8ffde708f65df
tomer-habusha <tomerhabusha@gmail.com>
2022-08-22 10:27:29 +0300

    Add MedicalImages dataset, channging root dir in main

--- dl-4-tsc-master/main.py
+++ dl-4-tsc-master/main.py
@@ -80,7 +80,7 @@ def create_classifier(classifier_name, input_shape, nb_classes, output_directory
 ############################################### main
 
 # change this directory for your machine
-root_dir = '/b/home/uha/hfawaz-datas/dl-tsc-temp/'
+root_dir = 'content/Deep_Learning_project/dl-4-tsc-master'
 
 if sys.argv[1] == 'run_all':
     for classifier_name in CLASSIFIERS:
